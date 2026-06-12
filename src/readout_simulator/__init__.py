7bc68acac52a6de2e46458708c5c16c976d212e9
jan-a-krzywda <j.a.krzywda@liacs.leidenuniv.nl>
2026-06-12 09:58:40 +0200

    clean

--- src/readout_simulator/__init__.py
+++ src/readout_simulator/__init__.py
@@ -12,11 +12,6 @@ from .quantum_dot_system import (
 )
 from .sensor_backend import RLC_sensor
 from .noise_models import OU_noise, OverFNoise, SpectrumNoise
-<<<<<<< HEAD
-=======
-from .jax_simulator import JAXReadoutSimulator
-from .dataset_tools import integrate_iq_traces, sample_integrated_iq, generate_sensor_iq_dataset
->>>>>>> b4e6f446a4938f45a85fe3a6d86fe6b056ab8cf4
 
 __version__ = "2.0.0"
 __author__ = "Jan A. Krzywda, Rouven Koch"
@@ -24,21 +19,8 @@ __author__ = "Jan A. Krzywda, Rouven Koch"
 __all__ = [
     "QuantumDotSystem",
     "GeometricQuantumDotSystem",
-<<<<<<< HEAD
     "RLC_sensor",
     "OU_noise",
     "OverFNoise",
     "SpectrumNoise",
 ]
-=======
-    "capacitance_matrices_to_geometry",
-    "RLC_sensor", 
-    "OU_noise",
-    "OverFNoise",
-    "SpectrumNoise",
-    "JAXReadoutSimulator",
-    "integrate_iq_traces",
-    "sample_integrated_iq",
-    "generate_sensor_iq_dataset",
-] 
->>>>>>> b4e6f446a4938f45a85fe3a6d86fe6b056ab8cf4
